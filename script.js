4f0105434b0ca93ddf387e68211b9477f32ec7de
mrunknow005 <mregale1234@gmail.com>
2025-10-12 22:27:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+alert("be careful")
+
+/*
+let b = prompt("enter the value")
+b=Number.parseInt(b)
+/*
+let c = confirm("do you wnat to delete ???")
+if (c) {
+  console.log(b)
+console.log(" The entered value type is " + typeof b)
+}
+else {
+  console.log("plese give me permistion")
+  document.write(b)
+}
+//document.write(b)
+*/
+//document.bgColor="red"
+//windows.console.log(ducument)
+// dom
+// aducument object broweser
+//console.log(ducument)
+// bom
+// broweser object
+// console.log(window)
+//let d = document.getElementsByClassName("cname");
+//console.log(d)
+
+/*
+console.log(document.body.firstChild); console.log(document.body.lastChild);
+let c = (document.body.childNodes);
+c= Array.from(c);
+console.log(c);
+*/
+// document.write("nice you wre nice have a good day");
+// document.title=("best");
+//document.bgColor= "lightblue";
+a = 34;
+b = 66;
+sum = a / b;
+console.log(sum);
+const t = [1,0, 7, 5, 87, 47];
+//t.pop();
+t.unshift(86);
+t.unshift(8654);
+
+t.pop();
+t.sort();
+console.log(t);
+t.reverse();
+w= document.getElementsByClassName("reset")
+console.log(w)
+//t.sort();
+//er the valu");
+// if (cc>=18) {
+//   console.log("you can drive");
+//   }
+// else {
+//   console.log("you can not drive");
+//}
