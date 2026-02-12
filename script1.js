8930765706dab0d589192ed69e5f40a4bf9eac88
mrunknow005 <mregale1234@gmail.com>
2026-02-12 20:20:10 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,49 @@
+alert("welcome to md site");
+//console.log("he")
+//a =prompt("enter you are name : ")
+///console.log(a);
+//b = confirm("This site some time use you are data ☠️☠️ ");
+//console.log(b)
+//document.title = "welcome to login page";
+/*
+a = prompt("hello");
+console.log(a);
+if (a>1 && a<9){
+  console.log("nice")
+}
+else
+if (a>10 && a<15) {
+  console.log("very nice")
+  
+} */
+//a = document.getElementsByClassName("sumbit");
+//console.log(a);
+ x = 14142.732
+x.toExponential(2);
+x.toExponential(3);
+console.log(x);
+let y = Date();
+//console.log(y)
+let v = Date(2024,2,7,5,7);
+//letsole.log(v);
+let d = Date("2027- 06-23");
+//console.log(d);
+//let x = setTime();
+console.log(x);
+let r = Location
+//letsole.log(r);
+s = Math.log10(2);
+console.log(s);
+k = Math.round(8.9999);
+//console.log(k)();
+j = Math.sqrt(2);
+//console.log(j)
+o = Math.max(2,5,6,6,764);
+//uonsole.log(o);
+u = Math.min(55,665,753,43,78);
+//console.log(u);
+function submit() {
+  window.location.reload();
+  window.location.href = "/12 sceince/one.html";
+   alert (" successfully submited");
+}
